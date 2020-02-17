82244cfe28e3e0343b9826bcbb8c972b25c0f445
Shivam Kumar <shivamkumar.dev23@gmail.com>
2020-02-18 00:15:12 +0530

    My frst Commit 2020

---
+++ inheritance.py
@@ -0,0 +1,21 @@
+class sohini():
+    def feature1(self):
+        print("cute")
+
+    def feature2(self):
+        print("sexy")
+
+class shivam(sohini):
+    def feature3(self):
+        print("hot")
+
+    def feature4(self):
+        print("loyal")
+
+
+
+a1=sohini()
+b1=shivam()
+
+b1.feature2()
+b1.feature4()
---
+++ leapyear.py
@@ -0,0 +1,3 @@
+print(*range(1, int(input())+1), sep='')
+
+
