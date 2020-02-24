4930c3d4de3faa8c2ffc9dd2371e5ecfec43f51f
Shivam Kumar <shivamkumar.dev23@gmail.com>
2020-02-24 09:56:12 +0530

    Second Commit

---
+++ counting using class and self.py
@@ -0,0 +1,11 @@
+class student:
+    count = 0
+    def __init__(self):
+        student.count=student.count+1
+
+s1=student()
+s2=student()
+s3=student()
+s4=student()
+
+print("the number of student is",student.count)
